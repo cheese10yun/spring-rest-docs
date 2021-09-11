b1a4a7bc78e2a72f91ccf89aad1125a80578a3c9
cheese10yun <cheese10yun@gmail.com>
2021-09-11 11:16:22 +0900

    Project init

---
+++ src/main/java/com/rest/docs/IndexController.java
@@ -0,0 +1,15 @@
+package com.rest.docs;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
