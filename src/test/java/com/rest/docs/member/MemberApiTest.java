1a66fbac6afa08da868f739ee62e2fc0c823127e
cheese10yun <cheese10yun@gmail.com>
2021-09-12 01:31:06 +0900

    Step 1

--- src/test/java/com/rest/docs/member/MemberApiTest.java
+++ src/test/java/com/rest/docs/member/MemberApiTest.java
@@ -1,49 +1,21 @@
 package com.rest.docs.member;
 
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.rest.docs.TestSupport;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@ExtendWith(RestDocumentationExtension.class)
-class MemberApiTest {
-
-    /**
-     * 1. Member 단일 조회 -> 완료
-     * 2. Member 생성 ->
-     * 3. Member 수정 ->
-     * 4. Member 페이징 조회 -> 완료
-     */
-
-    @Autowired
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp(
-        final WebApplicationContext context,
-        final RestDocumentationContextProvider provider
-    ) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
-            .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
-            .build();
-    }
+class MemberApiTest extends TestSupport {
 
     @Test
     public void member_page_test() throws Exception {
@@ -53,9 +25,15 @@ class MemberApiTest {
                     .param("page", "0")
                     .contentType(MediaType.APPLICATION_JSON)
             )
-            .andDo(print())
-            .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"))
             .andExpect(status().isOk())
+            .andDo(
+                restDocs.document(
+                    requestParameters(
+                        parameterWithName("size").optional().description("size"),
+                        parameterWithName("page").optional().description("page")
+                    )
+                )
+            )
         ;
     }
 
@@ -66,27 +44,40 @@ class MemberApiTest {
                 get("/api/members/{id}", 1L)
                     .contentType(MediaType.APPLICATION_JSON)
             )
-            .andDo(print())
-            .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"))
             .andExpect(status().isOk())
+            .andDo(
+                restDocs.document(
+                    pathParameters(
+                        parameterWithName("id").description("Member id")
+                    ),
+                    responseFields(
+                        fieldWithPath("id").description("Member id"),
+                        fieldWithPath("email").description("Email"),
+                        fieldWithPath("name").description("Name")
+                    )
+                )
+            )
+
         ;
     }
 
     @Test
     public void member_create() throws Exception {
         mockMvc.perform(
-            post("/api/members")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(""
-                    + "{\n"
-                    + "  \"email\": \"bbb@bbb.com\",\n"
-                    + "  \"name\": \"bbb\"\n"
-                    + "}")
-        )
-            .andDo(print())
-            .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"))
+                post("/api/members")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(readJson("/json/member-api/member-create.json"))
+            )
             .andExpect(status().isOk())
-            ;
+            .andDo(
+                restDocs.document(
+                    requestParameters(
+                        parameterWithName("size").optional().description("size"),
+                        parameterWithName("page").optional().description("page")
+                    )
+                )
+            )
+        ;
     }
 
     @Test
@@ -94,15 +85,18 @@ class MemberApiTest {
         mockMvc.perform(
                 put("/api/members/{id}", 1)
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content(""
-                        + "{\n"
-                        + "  \"name\": \"new-yun\"\n"
-                        + "}")
+                    .content(readJson("/json/member-api/member-modify.json"))
 
             )
-            .andDo(print())
-            .andDo(MockMvcRestDocumentation.document("{class-name}/{method-name}"))
             .andExpect(status().isOk())
+            .andDo(
+                restDocs.document(
+                    requestFields(
+                        fieldWithPath("name").description("name")
+                    )
+                )
+            )
+
         ;
     }
 }
